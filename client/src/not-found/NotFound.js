40725059da283f7ac03985a3b440b94ea86e382b
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-04-15 18:38:13 -0300

    Finalizando o not found

--- client/src/not-found/NotFound.js
+++ client/src/not-found/NotFound.js
@@ -1,12 +1,13 @@
 import { React } from "react";
 import Header from "../commons/Header";
 import Footer from "../commons/Footer";
+import "./NotFound.css";
 
 function NotFound(props) {
   return (
     <>
       <Header />
-      <main>
+      <main id="not-found-main">
         <h1>404 - Not Found</h1>
       </main>
       <Footer />
