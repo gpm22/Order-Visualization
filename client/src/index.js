546fd2457dccc44be22414b8892575b9e5c17627
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-04-16 13:32:52 -0300

    adicionando o redirecionamento de \ para orders

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Info from './info/Info';
-import OrdersPage from './orders/OrdersPage';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Info from "./info/Info";
+import OrdersPage from "./orders/OrdersPage";
 import NotFound from "./not-found/NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/"  element={<OrdersPage />} />
+      <Route path="/" element={<Navigate to="orders" replace/>} />
       <Route path="orders" element={<OrdersPage />} />
-      <Route path="info" element={<Info  />} />
+      <Route path="info" element={<Info />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
