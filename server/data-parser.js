3a334eb570f4cad8a2fc4e89f2f07a90a158735b
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-04-17 15:49:43 -0300

    tabela orders já vem com o nome do vendedor

--- server/data-parser.js
+++ server/data-parser.js
@@ -1,7 +1,15 @@
 const orders = require("./data/orders.json");
 const sellers = require("./data/sellers.json");
 
+const getSellerName = (sellerId) =>
+    sellers.filter((seller) => seller.id === sellerId)[0].name;
+
 module.exports = {
-    orders,
-    sellers
-}
+  orders: orders.map((order) => {
+    return {
+      ...order,
+      seller: getSellerName(order.seller),
+    };
+  }),
+  sellers,
+};
