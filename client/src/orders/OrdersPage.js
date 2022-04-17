047d85a6045a042a1836a1a29fc72047e297026a
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-04-16 22:31:25 -0300

    implementando e usando o http-requester

--- client/src/orders/OrdersPage.js
+++ client/src/orders/OrdersPage.js
@@ -1,17 +1,27 @@
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 import Footer from "../commons/Footer";
 import Header from "../commons/Header";
 import Orders from "./Orders";
 import OrdersTable from "./OrdersTable";
+import getObjects from "../utils/http-requester";
 
 function OrdersPage(props) {
+  let [objects, setObjects] = useState({ orders: false, sellers: false });
+
+  useEffect(() => {
+    async function fetchData() {
+      await getObjects(setObjects);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header />
       <main>
         <h1>Orders</h1>
-        <Orders />
-        <OrdersTable />
+        <Orders orders={objects.orders} sellers={objects.sellers} />
+        <OrdersTable orders={objects.orders} sellers={objects.sellers} />
       </main>
       <Footer />
     </>
--- client/src/utils/http-requester.js
+++ client/src/utils/http-requester.js
@@ -1,7 +1,27 @@
-import testValues from "./teste";
+const URL = "http://localhost:3200";
 
-const ordersObjects = {
-    ...testValues
-}
+const getObjects = async (setObjects) => {
+  fetch(URL, {
+    method: "get",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          setObjects(data);
+        });
+      } else {
+        throw new Error(
+          "Erro ao obter objetos do servidor. Status " + response.status
+        );
+      }
+    })
+    .catch((error) => {
+      throw new Error("Erro ao obter objetos do servidor.\n" + error);
+    });
+};
 
-export default ordersObjects;
+export default getObjects;
