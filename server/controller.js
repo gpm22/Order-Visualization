e358d740125edcbe57b8eb6f5790abf47420a7cb
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-04-16 22:31:46 -0300

    implementando o uso do cors

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const dataParser = require('./data-parser');
+const express = require("express");
+const dataParser = require("./data-parser");
+const cors = require("cors");
 
 const app = express();
 
 const port = 3200;
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.get("/", (req, res) => {
-    res.json(dataParser);
+  console.log(
+    `recebendo requisição de : ${
+      req.rawHeaders[req.rawHeaders.indexOf("Referer") + 1]
+    }`
+  );
+  res.json(dataParser);
 });
 
 app.listen(port, () => {
-    console.log(`Order Visualization server listening on port ${port}`);
+  console.log(`Order Visualization server listening on port ${port}`);
 });
