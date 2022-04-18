a91706f96a964ac55851601f9d7c5bdc1dfe5e3c
gpm22 <gabriel.milhomem@vorasys.com.br>
2022-04-18 16:44:54 -0300

    Implementando botões de início e de fim

--- client/src/orders/table/OrdesTableFooter.js
+++ client/src/orders/table/OrdesTableFooter.js
@@ -3,23 +3,30 @@ import React from "react";
 import "./OrdersTable.css";
 
 const TableFooter = ({ range, changePage, page }) => {
-
   const changePageLeft = () => {
-    return page > 1 ? page -1 : 1;
-  }
+    return page > 1 ? page - 1 : 1;
+  };
 
   const changePageRight = () => {
-    return page < range.length? page + 1 : page;
-  }
+    return page < range.length ? page + 1 : page;
+  };
 
   return (
     <div className="orders-table-footer">
-      <button
-        onClick={() => changePage(changePageLeft())}
-        className={`orders-table-footer-button orders-table-footer-button-border orders-table-footer-button-border-left`}
-      >
-        {"<<<"}
-      </button>
+      <div className="order-table-footer-buttoms-borders">
+        <button
+          onClick={() => changePage(changePageLeft())}
+          className={`orders-table-footer-button orders-table-footer-button-border orders-table-footer-button-border-left`}
+        >
+          {"\u25C0"}
+        </button>
+        <buton
+        onClick={() => changePage(1)}
+          className={`orders-table-footer-button orders-table-footer-button-border orders-table-footer-button-border-left`}
+        >
+          Older
+        </buton>
+      </div>
       {range.map((element, index) => (
         <button
           key={index}
@@ -31,12 +38,21 @@ const TableFooter = ({ range, changePage, page }) => {
           {element}
         </button>
       ))}
-      <button
-        onClick={() => changePage(changePageRight())}
-        className={`orders-table-footer-button orders-table-footer-button-border orders-table-footer-button-border-rigth`}
-      >
-        {">>>"}
-      </button>
+      <div className="order-table-footer-buttoms-borders">
+        <button
+          onClick={() => changePage(changePageRight())}
+          className={`orders-table-footer-button orders-table-footer-button-border orders-table-footer-button-border-rigth`}
+        >
+          {"\u25B6"}
+        </button>
+
+        <buton
+        onClick={() => changePage(range.length)}
+          className={`orders-table-footer-button orders-table-footer-button-border orders-table-footer-button-border-rigth`}
+        >
+          Newer
+        </buton>
+      </div>
     </div>
   );
 };
